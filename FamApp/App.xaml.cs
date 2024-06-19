eab5fce006f51b9080619fddb23263d94567e2b9
timea1304 <126492979+timea1304@users.noreply.github.com>
2024-06-19 19:45:53 +0200

    dimension angepasst auf handygröße

--- FamApp/App.xaml.cs
+++ FamApp/App.xaml.cs
@@ -8,6 +8,19 @@
 
             MainPage = new AppShell();
         }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
 
+            const int newWidth = 350;
+            const int newHeight = 800;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
+
 }
+
